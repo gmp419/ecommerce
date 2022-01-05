9ccd8037a5858e98023fb7b39cd677032684146f
gaurang <patel7483g@gmail.com>
2022-01-04 21:49:20 -0400

    finishing up setting beginning dependencies

---
+++ src/components/home/FeaturedProducts.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class FeaturedProducts extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default FeaturedProducts
---
+++ src/components/home/Slider.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Slider extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Slider
